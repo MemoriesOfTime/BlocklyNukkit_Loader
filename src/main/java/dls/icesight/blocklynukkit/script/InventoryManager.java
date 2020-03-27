85919a54ea37aa38b57102206446274504a61fdd
PenguinCaptain <1255676320@qq.com>
2020-03-27 19:34:02 +0800

    Problem with BlockEntityContainer

--- src/main/java/dls/icesight/blocklynukkit/script/InventoryManager.java
+++ src/main/java/dls/icesight/blocklynukkit/script/InventoryManager.java
@@ -1,50 +1,44 @@
 package dls.icesight.blocklynukkit.script;
 
-<<<<<<< HEAD
 import cn.nukkit.Player;
 import cn.nukkit.Server;
-import cn.nukkit.block.Block;
-import cn.nukkit.block.BlockChest;
-import cn.nukkit.block.BlockHopper;
-import cn.nukkit.block.BlockTrappedChest;
 import cn.nukkit.blockentity.BlockEntity;
 import cn.nukkit.blockentity.BlockEntityChest;
 import cn.nukkit.blockentity.BlockEntityContainer;
-import cn.nukkit.inventory.ContainerInventory;
+import cn.nukkit.blockentity.BlockEntityEnderChest;
+import cn.nukkit.inventory.BaseInventory;
 import cn.nukkit.inventory.Inventory;
 import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
+import cn.nukkit.math.Vector3;
 import com.nukkitx.fakeinventories.inventory.ChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.DoubleChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.FakeInventory;
 import dls.icesight.blocklynukkit.EventLoader;
+import dls.icesight.blocklynukkit.Loader;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class InventoryManager {
 
     public FakeInventory addInv(boolean isDoubleChest, Item[] item){
-        FakeInventory inv = addInv(isDoubleChest);
-        for(int i=0;i<inv.getSize()&&i<item.length;i++){
-            inv.setItem(i,item[i]);
-        }
-        inv.addListener(EventLoader::onSlotChange);
-        return inv;
-    }
-    public FakeInventory addInv(boolean isDoubleChest){
         FakeInventory inv;
         if (isDoubleChest){
             inv = new DoubleChestFakeInventory();
         }else{
             inv = new ChestFakeInventory();
         }
+        for(int i=0;i<inv.getSize()&&i<item.length;i++){
+            inv.setItem(i,item[i]);
+        }
+        inv.addListener(EventLoader::onSlotChange);
         return inv;
     }
     public void showFakeInv(Player player,FakeInventory inv){
         player.addWindow(inv);
     }
-    public Inventory editInv(Inventory inv, HashMap<Integer, Item> invContent){
+    public Inventory editInvByMap(Inventory inv, Map<Integer, Item> invContent){
         inv.setContents(invContent);
         return inv;
     }
@@ -54,29 +48,37 @@ public class InventoryManager {
         }
         return inv;
     }
-    public Inventory editInv(Inventory inv, int slot, Item item){
+    public Inventory editInvBySlot(Inventory inv, int slot, Item item){
         inv.setItem(slot,item);
         return inv;
     }
-    public Inventory getBlockInv(Position pos){
+    public FakeInventory getBlockInv(Position pos){
+        pos.fromObject(new Vector3(pos.getFloorX(),pos.getFloorY(),pos.getFloorZ()),pos.getLevel());
         BlockEntity blockEntity = pos.getLevel().getBlockEntity(pos);
         if(blockEntity instanceof BlockEntityContainer){
-            Inventory inv = null;
+            ChestFakeInventory inv = new ChestFakeInventory();
             for(int i=0;i<((BlockEntityContainer) blockEntity).getSize();i++){
                 inv.setItem(i,((BlockEntityContainer) blockEntity).getItem(i));
             }
+            blockEntity.close();
             return inv;
         }
         return null;
     }
-    public Inventory getPlayerInv(Player player){
-        Inventory inv = player.getInventory();
+    public void setBlockInv(Position pos, Inventory inv){
+        BlockEntity blockEntity = pos.getLevel().getBlockEntity(pos);
+        if(blockEntity instanceof BlockEntityContainer){
+            for(int i=0;i<inv.getSize();i++){
+                ((BlockEntityContainer) blockEntity).setItem(i,inv.getItem(i));
+            }
+        }
+    }
+    public FakeInventory getPlayerInv(Player player) {
+        DoubleChestFakeInventory inv = new DoubleChestFakeInventory();
+        inv.setContents(player.getInventory().getContents());
         return inv;
     }
-=======
-import cn.nukkit.item.Item;
-import cn.nukkit.level.Position;
-
-public class InventoryManager {
->>>>>>> 839f26fa055b7616c8ebc578a0f25d313600db46
+    public void setPlayerInv(Player player, Inventory inv){
+        player.getInventory().setContents(inv.getContents());
+    }
 }

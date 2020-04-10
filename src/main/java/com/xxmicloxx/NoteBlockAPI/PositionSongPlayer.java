de1f806d10540c0194c17612c07c7e87e58556db
超神的冰凉 <2801621420@qq.com>
2020-04-10 20:11:40 +0800

    红石音乐api

---
+++ src/main/java/com/xxmicloxx/NoteBlockAPI/PositionSongPlayer.java
@@ -0,0 +1,42 @@
+package com.xxmicloxx.NoteBlockAPI;
+
+import cn.nukkit.Player;
+import cn.nukkit.level.Location;
+
+public class PositionSongPlayer extends SongPlayer {
+
+    private Location targetLocation;
+
+    public PositionSongPlayer(Song song) {
+        super(song);
+    }
+
+    public Location getTargetLocation() {
+        return targetLocation;
+    }
+
+    public void setTargetLocation(Location targetLocation) {
+        this.targetLocation = targetLocation;
+    }
+
+    @Override
+    public void playTick(Player p, int tick) {
+        if (!p.getLevel().getName().equals(targetLocation.getLevel().getName())) {
+            // not in same world
+            return;
+        }
+        byte playerVolume = NoteBlockPlayerMain.getPlayerVolume(p);
+
+        for (Layer l : song.getLayerHashMap().values()) {
+            Note note = l.getNote(tick);
+            if (note == null) {
+                continue;
+            }
+            targetLocation.getLevel().addSound(
+                    targetLocation.getDirectionVector(),
+                    Instrument.getInstrument(note.getInstrument()),
+                    (l.getVolume() * (int) volume * (int) playerVolume) / 1000000f,
+                    NotePitch.getPitch(note.getKey() - 33));
+        }
+    }
+}
